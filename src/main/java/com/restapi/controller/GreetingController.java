7d7c5a554e38f87099dac8c54b9820033db98572
Teh Kok How <funcoolgeek@gmail.com>
2018-12-12 16:28:38 +0800

    (1) Use log.info (2) Add jmx.default-domain property

--- src/main/java/com/restapi/controller/GreetingController.java
+++ src/main/java/com/restapi/controller/GreetingController.java
@@ -1,8 +1,8 @@
 package com.restapi.controllers;
 import java.util.concurrent.atomic.AtomicLong;
-//import lombok.extern.slf4j.Slf4j;
-
-import org.apache.commons.logging.Log;
+/*import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+*/import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.restapi.models.Greeting;
 @RestController
-//@Slf4j
 @RequestMapping("/greeting")
 public class GreetingController {
-    private static final Log log = LogFactory.getLog(GreetingController.class);	
+    private static final Log log = LogFactory.getLog(GreetingController.class);
 	private static final String template = "Hello, %s!";
 	private static AtomicLong counter = new AtomicLong();
 	@GetMapping
 	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-		log.debug("GreetingController.greeting()");
+		log.info("GreetingController.greeting()");
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
